ca7b024a318cf1ba655aaa2af078beeef022a512
jojii <jojii@gmx.net>
2022-03-28 22:41:34 +0200

    add more helper fns

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::{cmp::Ordering, collections::BinaryHeap, marker::PhantomData};
 
 /// Priority container storing max `capacity` amount of items. Can be used to find
 /// `n` smallest items within an iterator or a set of items that implement `Ord`
@@ -195,5 +195,119 @@ impl<T: PartialEq + Eq> Ord for CustomOrd<T> {
     #[inline]
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.ord.cmp(&other.ord)
+    }
+}
+
+/// A struct to order values by a custom function
+pub struct OrderBy<T, U> {
+    val: T,
+    cmp_fn: U,
+}
+
+impl<T, U> OrderBy<T, U>
+where
+    U: Fn(&T, &T) -> Ordering,
+{
+    /// Create a new OrderBy
+    #[inline]
+    pub fn new(val: T, cmp_fn: U) -> Self {
+        Self { val, cmp_fn }
+    }
+
+    /// Convert back into `T`
+    #[inline]
+    pub fn into_inner(self) -> T {
+        self.val
+    }
+}
+
+impl<T, U> PartialEq for OrderBy<T, U>
+where
+    U: Fn(&T, &T) -> Ordering,
+{
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.partial_cmp(&other)
+            .map(|i| i == Ordering::Equal)
+            .unwrap_or(false)
+    }
+}
+
+impl<T, U> PartialOrd for OrderBy<T, U>
+where
+    U: Fn(&T, &T) -> Ordering,
+{
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some((self.cmp_fn)(&self.val, &other.val))
+    }
+}
+
+/// A struct to order values by a custom key function
+pub struct OrderByKey<T, U, K: Ord> {
+    val: T,
+    key_fn: U,
+    pd: PhantomData<K>,
+}
+
+impl<T, U, K: Ord> OrderByKey<T, U, K>
+where
+    U: Fn(&T) -> K,
+{
+    /// Create a new OrderBy
+    #[inline]
+    pub fn new(val: T, cmp_fn: U) -> Self {
+        Self {
+            val,
+            key_fn: cmp_fn,
+            pd: PhantomData,
+        }
+    }
+
+    /// Convert back into `T`
+    #[inline]
+    pub fn into_inner(self) -> T {
+        self.val
+    }
+}
+
+impl<T, U, K: Ord> PartialEq for OrderByKey<T, U, K>
+where
+    U: Fn(&T) -> K,
+{
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.partial_cmp(&other)
+            .map(|i| i == Ordering::Equal)
+            .unwrap_or(false)
+    }
+}
+
+impl<T, U, K: Ord> Eq for OrderByKey<T, U, K>
+where
+    U: Fn(&T) -> K,
+{
+    fn assert_receiver_is_total_eq(&self) {}
+}
+
+impl<T, U, K: Ord> PartialOrd for OrderByKey<T, U, K>
+where
+    U: Fn(&T) -> K,
+{
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let self_k = (self.key_fn)(&self.val);
+        let other_k = (other.key_fn)(&other.val);
+        Some(self_k.cmp(&other_k))
+    }
+}
+
+impl<T, U, K: Ord> Ord for OrderByKey<T, U, K>
+where
+    U: Fn(&T) -> K,
+{
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(&other).unwrap()
     }
 }
